1c43f714e76cca10d99a66e5fff4d7e3f07dfde9
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-05-05 00:36:24 +0200

    Fixed indexing bug which makes the white box and black box have unequal size

--- MakeFeaturePic.m
+++ MakeFeaturePic.m
@@ -8,12 +8,12 @@ function [ fpic ] = MakeFeaturePic( ftype, W, H )
         fpic(y:y+h-1, x:x+w-1) = -1;
         fpic(y+h:y+h+h-1, x:x+w-1) = 1;
     elseif ftype(1) == 2
-        fpic(y:y+h, x:x+w) = 1;
-        fpic(y:y+h, x+w:x+w+w) = -1;
+        fpic(y:y+h-1, x:x+w-1) = 1;
+        fpic(y:y+h-1, x+w:x+w+w-1) = -1;
     elseif ftype(1) == 3
-        fpic(y:y+h, x:x+w) = 1;
-        fpic(y:y+h, x+w:x+w+w) = -1;
-        fpic(y:y+h, x+w+w:x+w+w+w) = 1;
+        fpic(y:y+h-1, x:x+w-1) = 1;
+        fpic(y:y+h-1, x+w:x+w+w-1) = -1;
+        fpic(y:y+h-1, x+w+w:x+w+w+w-1) = 1;
     elseif ftype(1) == 4
         fpic(y:y+h-1, x:x+w-1) = 1;
         fpic(y:y+h-1, x+w:x+w+w-1) = -1;
