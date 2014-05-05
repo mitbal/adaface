fd81d4ec11ea013ca4e630a71c16ace2f8c02e63
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-05-06 01:23:50 +0200

    Added function ComputeROC that compute the ROC curve. However the output is still questionable. Alas, life goes on...

---
+++ ComputeROC.m
@@ -0,0 +1,65 @@
+function ComputeROC( Cparams, Fdata, NFdata )
+%function ComputeROC compute the ROC curve
+
+    face_fnames = dir(Fdata.dirname);
+    full_face = 3:length(face_fnames);
+    test_face = setdiff(full_face, Fdata.fnums);
+    num_tf = size(test_face,2);
+    
+    nface_fnames = dir(NFdata.dirname);
+    full_nface = 3:length(nface_fnames);
+    test_nface = setdiff(full_nface, NFdata.fnums);
+    num_tnf = size(test_nface,2);
+    
+    scores = zeros(num_tf+num_tnf, 2);
+    
+    for ii = 1:num_tf
+        im_fname = [Fdata.dirname, '/', face_fnames(test_face(ii)).name];
+        [~, ii_im] = LoadIm(im_fname);
+        sc = ApplyDetector(Cparams, ii_im);
+        scores(ii,1) = sc;
+        scores(ii,2) = 1;
+    end
+    
+    for ii = 1:num_tnf
+        im_fname = [NFdata.dirname, '/', nface_fnames(test_nface(ii)).name];
+        [~, ii_im] = LoadIm(im_fname);
+        sc = ApplyDetector(Cparams, ii_im);
+        scores(ii+num_tf,1) = sc;
+        scores(ii+num_tf,2) = 0;
+    end
+    
+    thresh = 0;
+    ntp = 0; nfp = 0; ntn = 0; nfn = 0;
+    
+    threshold = 0:0.01:10;
+    fpr = zeros(size(threshold));
+    tpr = zeros(size(threshold));
+    
+    for tt=1:size(threshold,2)
+        predicted_class = scores(:, 1) >= threshold(tt);
+        for ii=1:size(predicted_class, 1)
+            if predicted_class(ii) == 1 && scores(ii, 2) == 1
+                ntp = ntp+1;
+            elseif predicted_class(ii) == 0 && scores(ii, 2) == 0
+                ntn = ntn+1;
+            elseif predicted_class(ii) == 1 && scores(ii, 2) == 0
+                nfp = nfp+1;
+            elseif predicted_class(ii) == 0 && scores(ii, 2) == 1
+                nfn = nfn+1;
+            end
+        end
+
+        fpr(tt) = nfp / double(ntn+nfp);
+        tpr(tt) = ntp / double(ntp+nfn);
+        
+        if tpr(tt) > 0.7
+            thresh = threshold(tt);
+        end
+    end
+    
+    plot(fpr, tpr, 'r-'); axis([0 1 0 1]);
+    Cparams.thresh = thresh;
+
+end
+
