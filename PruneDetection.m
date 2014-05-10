639320c13ae823b367507ed1b4ef207ef106ea23
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-05-11 00:03:29 +0200

    Added function PruneDetection to reduce the number of detected faces in the same region to one

---
+++ PruneDetection.m
@@ -0,0 +1,32 @@
+function fdets = PruneDetection( dets )
+%function PruneDetection remove similar detection in the same region
+
+    nd = size(dets, 1);
+    D = zeros(nd, nd);
+    
+    rho = 0.75;
+    w = dets(1, 3);
+    h = dets(1, 4);
+    
+    % Check for intersection
+    for ii=1:nd
+        for jj=1:nd
+            intersect = rectint(dets(ii,:), dets(jj,:));
+            exclusive = (w*h + w*h - intersect);
+            if intersect / exclusive > rho
+                D(ii, jj) = 1;
+            end
+        end
+    end
+    
+    [S, C] = graphconncomp(sparse(D));
+    fdets = zeros(S, 4);
+    for ii=1:S
+        ind = C == ii;
+        boxes = dets(ind, :);
+        mbox = mean(boxes, 1);
+        fdets(ii, :) = mbox;
+    end
+
+end
+
