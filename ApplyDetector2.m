399121adcc5ea2d10818adcedbb1fef3bee7aa39
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-05-20 16:11:14 +0200

    Optimized function ApplyDetector2 by moving the classifier g inside the function

--- ApplyDetector2.m
+++ ApplyDetector2.m
@@ -5,15 +5,12 @@ function [ sc ] = ApplyDetector2( Cparams, ii_im, mu, sigma )
     p = Cparams.Thetas(:,3)';
     theta = Cparams.Thetas(:,2)';
     
-    %c_iiim = ii_im(y:y+h-1, x:x+w-1);
     c_iiim = ii_im;
     f = Cparams.Thetas(:, 1)';
     ftypes = Cparams.fmat(:, f);
     fs = double(c_iiim(:))' * ftypes;
+    
     % Feature Types III correction
-%     flag = (f > 10710 & f < 13924);
-%     w = 19; h = 19;
-%     fs = fs + double( uint8(flag) .* w*h*mu );
     ft = Cparams.all_ftypes(f, :);
     for ii=1:length(ft)
         if ft(ii, 1) == 3
@@ -22,6 +19,6 @@ function [ sc ] = ApplyDetector2( Cparams, ii_im, mu, sigma )
     end
     fs = fs ./ sigma;
     
-    sc = Cparams.alphas' * g(fs, p, theta)';
+    sc = Cparams.alphas' * (p .* fs < p .* theta)';
 
 end
